4005154f202e5c95cca8d2a8c64c00ece38153f7
Ian AS <i.silva97augusto@gmail.com>
2022-04-26 00:03:15 -0300

    Create style

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button } from "antd";
+import "antd/dist/antd.css";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Header">
+        <h1 className="title">Pomofocus</h1>
+        <div className="group-button-header">
+          <button>Report</button>
+          <button>Settings</button>
+          <button>Login</button>
+        </div>
+      </div>
+
+      <div className="timer-area">
+        <div>
+          <button className="group-button-timer">Pomodoro</button>
+          <button className="group-button-timer">Short Break</button>
+          <button className="group-button-timer">Long Break</button>
+        </div>
+        <div className="timer">25:00</div>
+
+        <Button>Start</Button>
+      </div>
     </div>
   );
 }
